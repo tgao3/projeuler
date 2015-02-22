3c937e15e0152c88d8dffc4abfa39addba3be530
tgao3 <tgao3@uic.edu>
2015-02-22 00:12:16 -0600

    p11 complete

--- 11.cpp
+++ 11.cpp
@@ -23,25 +23,26 @@ vector<string> read_digits(string name) {
 //convert the vector of strings to 2d-array of ints
 //need to parse the strings correctly with tokens
 int ** string_to_int(vector<string> name) {
-	istringstream isstream;
-	vector<vector<string>> number(20, vector<string>(20));
-	string temp;
-	int iter = 0;
-
-	for(string s : name) {
-		isstream.str(s);
-		cout << s << ' ';
-		while(getline(isstream, temp, ' ')){
-		//	cout << isstream.str() << endl;
-		//	cout << "here" << endl;
-		//	cout << temp << endl;
-		//	number[iter].push_back(temp);
-		//	cout << "iterere" << endl;
-		//	temp.clear();
-		}
-		iter++;
+	int ** grid = new int*[20];
+	for(int i = 0; i < 20; i++) {
+		grid[i] = new int[20];
 	}
 
+	istringstream s_to_int;
+	int row = 0, col = 0, temp;
+
+	for(string s : name) {
+		s_to_int.str(s);
+		s_to_int >> temp;
+		grid[row][col] = temp;
+		col++;
+		if(col == 20) {
+			row++;
+			col = 0;
+		}
+		s_to_int.clear();
+	}
+	return grid;
 }
 
 //finding the product
@@ -50,6 +51,31 @@ int ** string_to_int(vector<string> name) {
 //
 //direction parameter: 0 for right, 1 for down-right, 2 for down, 3 for up-right
 int calculate(int ** array, int row, int col, int direction) {
+		int count = 0;
+		uint64_t result = 1;
+		while(row > -1 && col > -1 &&
+			row < 20 && col < 20) {
+
+			if(!direction) {
+				result *= array[row][col];
+				col++;
+			} else if(direction == 1) {
+				result *= array[row][col];
+				row++;
+				col++;
+			} else if(direction == 2) {
+				result *= array[row][col];
+				row++;
+			} else if(direction == 3) {
+				result *= array[row][col];
+				row--;
+				col++;
+			}
+			count++;
+			if(count == 4) return result;
+		}
+	
+		return 0;
 }
 
 
@@ -60,5 +86,24 @@ int main() {
 	
 	int ** numbers;
 	numbers = string_to_int(grid);
+
+	uint64_t highest = 0;
+	uint64_t r = 0, dr = 0, d = 0, ur = 0;
+
+	for(int i = 0; i < 20; i++) {
+		for(int j = 0; j < 20; j++) {
+			r = calculate(numbers, i, j, 0);
+			dr = calculate(numbers, i, j, 1);
+			d = calculate(numbers, i, j, 2);
+			ur = calculate(numbers, i, j, 3);
+	
+			if(r > highest) highest = r;
+			if(dr > highest) highest = dr;
+			if(d > highest) highest = d;
+			if(ur > highest) highest = ur;
+		}
+	}
+
+	cout << highest << endl;
 	return 0;
 }
